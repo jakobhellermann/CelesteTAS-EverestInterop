72a4a6637f24d51638e02524e931c32e98d8c887
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-03-13 09:20:16 +0100

    ninesols: savestates using DebugModPlus

--- CelesteTAS-EverestInterop/Source/GlobalUsings.cs
+++ CelesteTAS-EverestInterop/Source/GlobalUsings.cs
@@ -1,10 +1,12 @@
 global using static TAS.GlobalVariables;
+using TAS.ModInterop;
 using TAS.Module;
 
 namespace TAS;
 
 public static class GlobalVariables {
     public static CelesteTasSettings TasSettings => TasMod.Instance.TasSettings;
+    public static DebugModPlusInteropGlue? DebugModPlusInterop => TasMod.Instance.DebugModPlusInterop;
 
     public static void AbortTas(string message, bool log = false, float duration = 2f) {
         Log.Toast($"Aborting TAS: {message}");
